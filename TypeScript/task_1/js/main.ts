e8bc82ce658876714251ffd79ae0c281eed9f7b1
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-19 09:39:09 +0200

    extending teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -2,17 +2,18 @@ interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
 }
  
-const teacher3: Teacher = {
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
   firstName: 'John',
-  fullTimeEmployee: false,
   lastName: 'Doe',
   location: 'London',
-  contract: false,
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
- 
-console.log(teacher3);
+console.log(director1);
